a4d53265e6fcda3ba3e0cae29716db76814e1115
Douglas Silva <dogusky@protonmail.com>
2024-07-31 01:18:31 -0300

    refact: remove dead code

--- bibliothecarius/models/canon.py
+++ bibliothecarius/models/canon.py
@@ -1,17 +1,17 @@
 from typing import List
 from bibliothecarius.models.base import Base
 from sqlalchemy.orm import mapped_column, Mapped, relationship
-from sqlalchemy import Column, ForeignKey, String, Integer, Table
+from sqlalchemy import ForeignKey, String, Integer
 from bibliothecarius.models.book import Book
 
 
-associative_table = Table(
-    "book_canon",
-    Base.metadata,
-    Column("canon_id", ForeignKey("canons.canon_id")),
-    Column("book_id", ForeignKey("books.book_id")),
-    Column("sort_index", Integer)
-)
+class BookCanon(Base):
+    __tablename__ = "book_canon"
+    book_canon_id: Mapped[int] = mapped_column(primary_key=True) 
+    canon_id: Mapped[int] = mapped_column(ForeignKey("canons.canon_id"))
+    book_id: Mapped[int] = mapped_column(ForeignKey("books.book_id"))
+    sort_index: Mapped[int]
+    child: Mapped["Book"] = relationship()
 
 
 class Canon(Base):
@@ -22,7 +22,7 @@ class Canon(Base):
     tradition: Mapped[str] = mapped_column(String, nullable=False)
     total_books: Mapped[int] = mapped_column(Integer, nullable=False)
 
-    books: Mapped[List[Book]] = relationship(secondary=associative_table)
+    books: Mapped[List["BookCanon"]] = relationship()
 
     def __repr__(self) -> str:
         return f"{self.name} ({self.tradition} - {self.total_books})"
